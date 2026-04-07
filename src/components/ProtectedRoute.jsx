dbdf15bd6d147ef2ec5a7bc975332d14e79ef3f0
shubham kumar <shubhamfarainzi@gmail.com>
2026-04-07 22:38:56 +0530

    updated

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,17 +1,43 @@
+// import { useSelector } from "react-redux";
+// import { Navigate } from "react-router-dom";
+
+// const ProtectedRoute = ({ children }) => {
+//   const { login, isLoading } = useSelector((state) => state.auth);
+
+//   // ⏳ WAIT until API finishes
+//   if (isLoading) {
+//     return <div className="text-white text-center mt-10">Loading...</div>;
+//   }
+
+//   // ❌ only redirect AFTER checking
+//   if (!login) {
+//     return <Navigate to="/login" replace />;
+//   }
+
+//   return children;
+// };
+
+// export default ProtectedRoute;
+
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
-  const { login, isLoading } = useSelector((state) => state.auth);
+  const { login, isLoading, user } = useSelector((state) => state.auth);
 
-  // ⏳ WAIT until API finishes
+  // ⏳ wait until API completes
   if (isLoading) {
     return <div className="text-white text-center mt-10">Loading...</div>;
   }
 
-  // ❌ only redirect AFTER checking
+  // ❌ not logged in
   if (!login) {
     return <Navigate to="/login" replace />;
+  }
+
+  // ⛔ user अभी तक load नहीं हुआ
+  if (!user?._id) {
+    return <div className="text-white text-center mt-10">Preparing user...</div>;
   }
 
   return children;
